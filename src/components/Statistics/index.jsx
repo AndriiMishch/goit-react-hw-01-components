db086c67a33b9945fb05c6a16272f760d295fbd8
AndriiMishch <anderito02@gmail.com>
2023-09-26 18:06:22 +0200

    Create index.jsx

---
+++ src/components/Statistics/index.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import {
+  StatisticsSection,
+  StatisticsTitle,
+  StatisticsList,
+  StatisticsListItem,
+} from './Statistic.styled';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <StatisticsSection>
+      {title && <StatisticsTitle>{title}</StatisticsTitle>}
+
+      <StatisticsList>
+        {stats.map(stat => {
+          return (
+            <StatisticsListItem key={stat.id}>
+              <span>{stat.label}</span>
+              <span>{stat.percentage}%</span>
+            </StatisticsListItem>
+          );
+        })}
+      </StatisticsList>
+    </StatisticsSection>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
