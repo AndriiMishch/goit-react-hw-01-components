c97b0831bd54df16bcfd70403f6d7293ea723153
AndriiMishch <anderito02@gmail.com>
2023-09-26 18:06:27 +0200

    Create index.jsx

---
+++ src/components/TransactionHistory/index.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { Table, TableHead, TableBody } from './TransactionHistory.styled';
+
+export default function TransactionHistory({ transactions }) {
+  return (
+    <Table>
+      <TableHead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </TableHead>
+      <TableBody>
+        {transactions.map(transaction => {
+          return (
+            <tr key={transaction.id}>
+              <td>{transaction.type}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.currency}</td>
+            </tr>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
